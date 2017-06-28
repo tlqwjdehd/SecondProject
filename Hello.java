7929d03b5a7f80a8ae9d823583602a353c55532f
tlqwjdehd <01033634052@naver.com>
2017-06-28 17:33:03 +0900

    added output "That's DVCS"

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("What is Git?");
+		System.out.println("That's DVCS");
 
 	}
 }
